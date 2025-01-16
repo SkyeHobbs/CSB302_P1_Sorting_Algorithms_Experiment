cd6b74621118fa0a24022e6aba166cd2b6690e41
HJStaiff <henrystaiff@gmail.com>
2025-01-16 13:54:38 -0800

    Added classes for array generation and sorting algorithms.

---
+++ src/test/java/SortingAlgorithm/InsertionSortTest.java
@@ -0,0 +1,12 @@
+package SortingAlgorithm;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InsertionSortTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
