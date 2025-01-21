2014cb5a2f0b110c33387605a964356ddf96b0db
HJStaiff <henrystaiff@gmail.com>
2025-01-20 18:40:26 -0800

    Added ordered array and reverse ordered array generation. Added test classes for ordered and reverse ordered arrays.

--- src/test/java/arraygeneration/OrderedArrayTest.java
+++ src/test/java/arraygeneration/OrderedArrayTest.java
@@ -1,12 +1,29 @@
 package arraygeneration;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderedArrayTest {
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void testOrderedArray() {
+        OrderedArray orderedArray = new OrderedArray();
+        int[] testArray = orderedArray.generateArray(100);
+        assertEquals(100, testArray.length);
+
+        int[] sortedArray = Arrays.copyOf(testArray, testArray.length);
+        Arrays.sort(sortedArray);
+        assertArrayEquals(sortedArray, testArray);
+    }
+
+    @Test
+    void testOrderedArrayZeroLength() {
+        OrderedArray orderedArray = new OrderedArray();
+        int[] array = orderedArray.generateArray(0);
+        assertEquals(0, array.length);
     }
 }
