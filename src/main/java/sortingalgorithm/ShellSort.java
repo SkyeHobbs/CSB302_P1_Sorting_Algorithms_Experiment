9d5aa88dbea5183f9cb16435caff1f4ed937f736
HJStaiff <henrystaiff@gmail.com>
2025-01-23 15:35:38 -0800

    Added counting sort, and shell sort outline. Added javadoc for time complexity, stability and in place of merge sort counting sort and shellsort.

--- src/main/java/sortingalgorithm/ShellSort.java
+++ src/main/java/sortingalgorithm/ShellSort.java
@@ -1,4 +1,26 @@
 package sortingalgorithm;
 
-public class ShellSort {
+/**
+ * Shell sort class. Sorts a generic array.
+ *
+ * @param <T> the type of the array to sort.
+ */
+public class ShellSort<T extends Comparable<T>> {
+
+  /**
+   * Shell sort algorithm. Sorts an array of generic type.
+   * Time complexity of O(n^2)
+   * Shell sort is an unstable sorting algorithm.
+   * Shell sort is done in place.
+   *
+   * @param array the array to sort.
+   * @return the sorted array.
+   */
+  public T[] shellSort(T[] array) {
+
+
+
+
+    return array;
+  }
 }
