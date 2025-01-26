b85793e1e2863a095962580d2fb71836d0a61772
user.email <skyeguy2003@gmail.com>
2025-01-26 13:14:59 -0800

    Fifty Percent Ordered Array class and test

--- src/test/java/arraygeneration/FiftyPercentOrderedArrayTest.java
+++ src/test/java/arraygeneration/FiftyPercentOrderedArrayTest.java
@@ -1,12 +1,32 @@
 package arraygeneration;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class FiftyPercentOrderedArrayTest {
+  @Test
+  public void fiftyPercentOrderedArray() {
+    FiftyPercentOrderedArray arr = new FiftyPercentOrderedArray();
 
-    //@BeforeEach
-    void setUp() {
+    int[] fiftyPercentOrderedArray = arr.fiftyPercentOrderedArray(0);
+    assertEquals(fiftyPercentOrderedArray.length, 0);
+
+    fiftyPercentOrderedArray = arr.fiftyPercentOrderedArray(10);
+    assertEquals(fiftyPercentOrderedArray.length, 10);
+
+    for (int i = 0; i < (fiftyPercentOrderedArray.length / 2) - 1; i++) {
+      assertTrue(fiftyPercentOrderedArray[i] <= fiftyPercentOrderedArray[i + 1]);
     }
+
+    fiftyPercentOrderedArray = arr.fiftyPercentOrderedArray(1000);
+    assertEquals(fiftyPercentOrderedArray.length, 1000);
+
+    for (int i = 0; i < (fiftyPercentOrderedArray.length / 2) - 1; i++) {
+      assertTrue(fiftyPercentOrderedArray[i] <= fiftyPercentOrderedArray[i + 1]);
+    }
+  }
+
 }
