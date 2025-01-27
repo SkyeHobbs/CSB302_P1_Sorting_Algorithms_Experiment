296cbf8ac0136a44e4a0582d5d9bb25ec7c4978e
user.email <skyeguy2003@gmail.com>
2025-01-27 13:30:13 -0800

    Seventy five percent ordered array

--- src/main/java/arraygeneration/SeventyFivePercentOrderedArray.java
+++ src/main/java/arraygeneration/SeventyFivePercentOrderedArray.java
@@ -1,4 +1,34 @@
 package arraygeneration;
 
+import java.util.Arrays;
+
+/**
+ * Fifty Percent Ordered Array Class. For an integer array
+ */
 public class SeventyFivePercentOrderedArray {
+  /**
+   * This method creates an integer array from the given
+   * size of random integer values. Then sorts the first seventy-five
+   * of the array in ascending order before returning the whole array.
+   *
+   * @param length the size of the array.
+   * @return int[] an array of integers that is seventy-five percent sorted
+   */
+  public int[] seventyFivePercentOrderedArray(int length) {
+    // Check if the given length is 0
+    if (length <= 0) {
+      return new int[0];
+    } else {
+      RandomOrderedArray arr = new RandomOrderedArray();
+
+      // Create an array from the given length of random integer values
+      int[] result = arr.generateArray(length);
+      int seventyPercent = (length / 2) + (length / 4);
+
+      // Sort the first seventy-five percent of the array
+      Arrays.sort(result, 0, seventyPercent);
+
+      return result; // Return the seventy-five percent sorted array
+    }
+  }
 }
--- src/test/java/arraygeneration/SeventyFivePercentOrderedArrayTest.java
+++ src/test/java/arraygeneration/SeventyFivePercentOrderedArrayTest.java
@@ -1,12 +1,31 @@
 package arraygeneration;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SeventyFivePercentOrderedArrayTest {
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    public void seventyFivePercentOrderedArray() {
+        SeventyFivePercentOrderedArray arr = new SeventyFivePercentOrderedArray();
+
+        int[] seventyFivePercentOrderedArray = arr.seventyFivePercentOrderedArray(0);
+        assertEquals(seventyFivePercentOrderedArray.length, 0);
+
+        seventyFivePercentOrderedArray = arr.seventyFivePercentOrderedArray(10);
+        assertEquals(seventyFivePercentOrderedArray.length, 10);
+
+        for (int i = 0; i < ((seventyFivePercentOrderedArray.length / 2) + (seventyFivePercentOrderedArray.length / 4)) - 1; i++) {
+            assertTrue(seventyFivePercentOrderedArray[i] <= seventyFivePercentOrderedArray[i + 1]);
+        }
+
+        seventyFivePercentOrderedArray = arr.seventyFivePercentOrderedArray(1000);
+        assertEquals(seventyFivePercentOrderedArray.length, 1000);
+
+        for (int i = 0; i < ((seventyFivePercentOrderedArray.length / 2) + (seventyFivePercentOrderedArray.length / 4)) - 1; i++) {
+            assertTrue(seventyFivePercentOrderedArray[i] <= seventyFivePercentOrderedArray[i + 1]);
+        }
     }
 }
