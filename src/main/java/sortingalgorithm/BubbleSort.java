886aea0b3f5975fbca4998a2be2ea8361bfedcb3
Tinisha <tinishadavis159@gmail.com>
2025-01-28 17:25:05 -0800

    Adding BubbleSort and test

--- src/main/java/sortingalgorithm/BubbleSort.java
+++ src/main/java/sortingalgorithm/BubbleSort.java
@@ -1,4 +1,37 @@
 package sortingalgorithm;
 
+import java.util.ArrayList;
+
+/**
+ * A utility class that provides a generic method to perform bubble sort on an {@link ArrayList}.
+ */
 public class BubbleSort {
+  /**
+   * Sorts the specified {@link ArrayList} using the bubble sort algorithm. The elements in the list
+   * must implement the {@link Comparable} interface.
+   *
+   * @param <T>   the type of elements in the list, which must implement {@link Comparable}
+   * @param array the {@link ArrayList} to be sorted
+   * @throws NullPointerException if the specified list is null or contains null elements
+   */
+  public static <T extends Comparable<T>> void bubbleSort(ArrayList<T> array) {
+    int n = array.size();
+    boolean swapped;
+    for (int i = 0; i < n - 1; i++) {
+      swapped = false;
+      for (int j = 0; j < n - i - 1; j++) {
+        if (array.get(j).compareTo(array.get(j + 1)) > 0) {
+          // Swap array[j] and array[j+1]
+          T temp = array.get(j);
+          array.set(j, array.get(j + 1));
+          array.set(j + 1, temp);
+          swapped = true;
+        }
+      }
+      // If no two elements were swapped, break the loop
+      if (!swapped) {
+        break;
+      }
+    }
+  }
 }
--- src/test/java/sortingalgorithm/BubbleSortTest.java
+++ src/test/java/sortingalgorithm/BubbleSortTest.java
@@ -1,12 +1,34 @@
 package sortingalgorithm;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BubbleSortTest {
+    private ArrayList<Integer> array1;
+    private ArrayList<String> array2;
 
     @BeforeEach
     void setUp() {
+        array1 = new ArrayList<>(Arrays.asList(64, 34, 25, 12, 22, 11, 90));
+        array2 = new ArrayList<>(Arrays.asList("pear", "apple", "orange", "banana"));
+
+    }
+
+    @Test
+    void testBubbleSort() {
+
+        ArrayList<Integer> expected1 = new ArrayList<>(Arrays.asList(11, 12, 22, 25, 34, 64, 90));
+        ArrayList<String> expected2 = new ArrayList<>(Arrays.asList("apple", "banana", "orange", "pear"));
+
+        BubbleSort.bubbleSort(array1);
+        BubbleSort.bubbleSort(array2);
+
+        assertEquals(expected1, array1);
+        assertEquals(expected2, array2);
     }
 }
